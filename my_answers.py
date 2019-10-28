5b7922d7b70640d945abc2599a15ab300c690ac5
Shahzina Khan <shahzinakhan@gmail.com>
2019-10-27 23:23:41 -0700

    validation loss trial

--- my_answers.py
+++ my_answers.py
@@ -142,7 +142,7 @@ class NeuralNetwork(object):
 #########################################################
 # Set your hyperparameters here
 ##########################################################
-iterations = 100
-learning_rate = 0.1
-hidden_nodes = 2
+iterations = 4000
+learning_rate = 0.081
+hidden_nodes = 45
 output_nodes = 1
